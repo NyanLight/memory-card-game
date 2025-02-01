85764f2e2181b33c52fcf32869bdde8f241493bb
NyanLight <128150876+NyanLight@users.noreply.github.com>
2025-02-01 20:25:06 +0400

    create function to fetch pokemon api

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+export function App() {
+  useEffect(() => {
+    
+  }, [])
+
+  return (
+    <>
+    </>
+  )
+}
+
---
+++ src/api/fetchPokemon.tsx
@@ -0,0 +1,13 @@
+export async function fetchPokemon(id: number) {
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        const json =  await response.json();
+        const pokemon = {
+            name: json.name,
+            image: json.sprites.front_default,
+        }
+        return pokemon;
+    } catch (error) {
+        console.error(error);
+    }
+}
